5f2800c334c9c0d7459e414af58029782ed38b57
Mativec <97987898+Mativec@users.noreply.github.com>
2022-02-18 15:33:45 +0100

    Séances / Fin du chrono simple

---
+++ ChronoSimple.c
@@ -0,0 +1,96 @@
+/**
+ * @file Chrono.c
+ * @author TWARDAWA Yanis & VECCHIO Matias
+ * @brief Main du programme pour le TP2 de ProgC
+ * @version 0.1
+ * @date 2022-02-18
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <ncurses.h>
+
+
+/**
+ * @brief fonction qui renvoie le tmeps entre deux temps donné en 
+ * paramètre, en milliseconde
+ * 
+ * @param debut le temps en microseconde
+ * @param fin le temps en microseconde
+ * @return int 
+ */
+int intervalle(struct timeval debut, struct timeval fin){
+    return ((fin.tv_sec - debut.tv_sec) * 1000) + ((fin.tv_usec - debut.tv_usec) / 1000);
+}
+
+
+/**
+ * @brief Fonction qui renvoie nb_ms en centiemes de secondes.
+ * 
+ * @param nb_ms: nombre de millisecondes. 
+ * @return int: nombre de millisecondes convertis en centieme de secondes.
+ */
+int nb_ms_vers_centiemes(int nb_ms) {
+    return (nb_ms / 10) % 100;
+}
+
+
+/**
+ * @brief Fonction qui renvoie nb_ms en secondes.
+ * 
+ * @param nb_ms: nombre de millisecondes.
+ * @return int: nombre de millisecondes convertis en secondes.
+ */
+int nb_ms_vers_secondes(int nb_ms){
+    return (nb_ms / 1000) % 60;
+}
+
+
+/**
+ * @brief Fonction qui renvoie nb_ms en secondes.
+ * 
+ * @param nb_ms: nombre de millisecondes.
+ * @return int: nombre de millisecondes convertis en minutes.
+ */
+int nb_ms_vers_minutes(int nb_ms) {
+    return (nb_ms / (1000 * 60)) % 60;
+}
+
+
+/**
+ * @brief Fonction qui renvoie nb_ms en secondes.
+ * 
+ * @param nb_ms: nombre de millisecondes.
+ * @return int: nombre de millisecondes convertis en heures.
+ */
+int nb_ms_vers_heures(int nb_ms) {
+    return (nb_ms / (1000 * 60 * 60));
+}
+
+
+int main(void) {
+    
+    /* Variables de recuperation du temps. */
+    struct timeval debut_prog, actuel;
+
+    /* Variables pour recueillir les mesures. */
+    int chrono;
+
+    /* Mesure de temps initiale. */
+    gettimeofday(&debut_prog, NULL);
+    chrono = 0;
+
+
+    while(1) {
+        gettimeofday(&actuel, NULL);
+        chrono = intervalle(debut_prog, actuel);
+        printf("%d : %d : %d : %d\n", nb_ms_vers_heures(chrono), nb_ms_vers_minutes(chrono), nb_ms_vers_secondes(chrono), nb_ms_vers_centiemes(chrono));
+        usleep(500000);
+    }
+    return 0;
+}
